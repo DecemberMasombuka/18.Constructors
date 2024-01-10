e6415a15049e99957688c1d3ff630f1e7e0c1999
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:54:26 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Book
+    {
+        public string title;
+        public string description;
+        public string author;
+        public int  pages;
+
+        //Constructor for the class
+        public Book (string atitle, string adescription, string author, int apages)
+        {
+            //Initialising variables
+            title = atitle;
+            description = adescription;
+            this.author = author;
+            pages = apages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling the constructor and passing its appropriate parameters
+            Book book1 = new Book("God of good","Book about good of good","Makhanda",5544);
+            Book book2 = new Book("Impossible is void", "Discusses the motivation needed", "Tim Sukile", 6447);
+          
+            Display(book1);
+            Display(book2);
+
+
+            Console.Read();
+        }
+
+        //Display fucntion
+        static void Display(Book book)
+        {
+            Console.WriteLine(book.title);
+            Console.WriteLine(book.author);
+            Console.WriteLine(book.description);
+            Console.WriteLine(book.pages + "  Pages");
+
+            Console.WriteLine();
+        }
+    }
+}
